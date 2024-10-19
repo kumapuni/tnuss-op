f1529858b1b7ae8d7b569155f620a15a32ae8033
kumapuni <161445059+kumapuni@users.noreply.github.com>
2024-10-19 19:14:47 +0900

    file

---
+++ data/script.js
@@ -0,0 +1,9 @@
+   $(function () {
+  const hamburger = $(".hamburger");
+  const nav = $(".nav");
+
+  hamburger.click(function () {
+    $(this).find(".hamburger_bar").toggleClass("is_active");
+    nav.toggleClass("is_active");
+  });
+});
